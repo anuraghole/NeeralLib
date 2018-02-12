43377d05f4178ad9a6b10ffa6eb7f7fbea7f4259
RajatDGupta <ngupta.gupta53@gmail.com>
2018-02-12 17:19:58 +0530

    neeraj commit

---
+++ neeraj-lib/src/main/java/neeraj/example/com/neerajlib/NeerajClass.java
@@ -0,0 +1,20 @@
+package neeraj.example.com.neerajlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Neeraj on 12-Feb-18.
+ */
+
+public class NeerajClass {
+    Context context;
+
+    NeerajClass(Context context){
+        this.context=context;
+    }
+
+    void showMessage(String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
